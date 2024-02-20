3f9b7bb826dba6f679a90d95ac409dd75e989c30
Alina Luzanova <alinaluzanova@gmail.com>
2024-02-20 18:21:27 +0300

    Add earth, save button (don't work, just exist)

--- client/src/components/RouteCard.tsx
+++ client/src/components/RouteCard.tsx
@@ -4,6 +4,10 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import styles from '../styles/Main.module.css';
+import Rating from '@mui/material/Rating';
+import Checkbox from '@mui/material/Checkbox';
+import FavoriteBorder from '@mui/icons-material/FavoriteBorder';
+import Favorite from '@mui/icons-material/Favorite';
 interface Route {
     id: number;
     title: string;
@@ -17,8 +21,11 @@ interface Route {
 const RouteCard: React.FC<{ route: Route }> = ({ route }) => {
     return (
         <div className={styles.routeCard}>
+
             <Card sx={{ minWidth: 300, borderRadius: 10, margin: 2, padding: 3, maxWidth: 300 }}>
                 <CardContent>
+                    <div className={styles.cardContainer}>
+                    <div>
                     <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
                         {route.city}
                         <br />
@@ -35,8 +42,15 @@ const RouteCard: React.FC<{ route: Route }> = ({ route }) => {
                     <Typography variant="body2">
                         Rating: {route.rating}
                         <br />
+                        <Rating name="half-rating" className='rating' defaultValue={route.rating} precision={0.1} readOnly/>
+                        <br />
                         Distance: {route.longway}
                     </Typography>
+                    </div>
+                        <div className={styles.favourite}>
+                            <Checkbox icon={<FavoriteBorder />} checkedIcon={<Favorite />} style={{color:'red'}}/>
+                        </div>
+        </div>
                 </CardContent>
                 <CardActions>
                     <a href='#'>Learn More</a>
