1df41b687da005099640d0485806acbd34581f43
kuzmenko.ov <viper-auto@yandex.ru>
2024-02-20 10:32:17 +0300

    Project begin tune 3

--- server/routes/index.routes.js
+++ server/routes/index.routes.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 
-const mainRouter = require('./main.routes'); // TODO
-const authRouter = require('./auth.routes'); // TODO
+// const mainRouter = require('./main.routes'); // TODO
+// const authRouter = require('./auth.routes'); // TODO
 
-router.use('/', mainRouter); // TODO
-router.use('/auth', authViewRouter); // TODO
+// router.use('/', mainRouter); // TODO
+// router.use('/auth', authViewRouter); // TODO
 
 module.exports = router;
